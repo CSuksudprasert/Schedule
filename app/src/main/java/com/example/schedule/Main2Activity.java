fb5f4689754e516201d48e84e992c893b0f10dbd
eveyosita <yositabp@gmail.com>
2018-12-14 19:47:22 +0700

    commit 4

--- app/src/main/java/com/example/schedule/Main2Activity.java
+++ app/src/main/java/com/example/schedule/Main2Activity.java
@@ -1,20 +1,12 @@
 package com.example.schedule;
 
 import android.content.ContentValues;
-import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Spinner;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -22,9 +14,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.List;
 
-import static com.example.schedule.DatabaseHelper.COL_CLASS;
 import static com.example.schedule.DatabaseHelper.COL_DAY;
 import static com.example.schedule.DatabaseHelper.COL_SUB;
 import static com.example.schedule.DatabaseHelper.COL_TIME;
@@ -37,11 +27,9 @@ public class Main2Activity extends AppCompatActivity {
     private DatabaseHelper mHelper;
     private SQLiteDatabase mDb;
 
-    Spinner daySpin;
-    Spinner timeS;
-
-    String arrday[] = {"Monday","Tuesday","Wednesday","Thursday","Friday"};
-    String arrtime[] = {"08.30-10.15","10.20-12.05","13.00-14.45","14.50-16.35"};
+    EditText subject1editText;
+    EditText dayeditText;
+    EditText timeeditText;
 
 
     @Override
@@ -49,20 +37,12 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        daySpin =  (Spinner) findViewById(R.id.day1_spinner);
-        timeS =  (Spinner) findViewById(R.id.timeStart_spinner);
         mHelper = new DatabaseHelper(this);
         mDb = mHelper.getWritableDatabase();
 
-        ArrayAdapter<String> adapterDay = new ArrayAdapter<String>(this,
-                android.R.layout.simple_dropdown_item_1line, arrday);
-        daySpin.setAdapter(adapterDay);
 
-        ArrayAdapter<String> adapterTimeS = new ArrayAdapter<String>(this,
-                android.R.layout.simple_dropdown_item_1line, arrtime);
-        timeS.setAdapter(adapterTimeS);
 
-        Button addbutton = findViewById(R.id.add_button);
+        Button addbutton = findViewById(R.id.add2_button);
         addbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -93,13 +73,13 @@ public class Main2Activity extends AppCompatActivity {
     }
 
     private void doInsertItem_class() {
-        EditText subject1editText = findViewById(R.id.subject1_editText);
-        Spinner day1spinner = findViewById(R.id.day1_spinner);
-        Spinner timeStartpinner =findViewById(R.id.timeStart_spinner);
+        subject1editText = findViewById(R.id.subject1_editText);
+        dayeditText =  findViewById(R.id.day_editText);
+        timeeditText = findViewById(R.id.time_editText);
 
         String subject = subject1editText.getText().toString();
-        String day1 = day1spinner.toString();
-        String time = timeStartpinner.toString();
+        String day1 = dayeditText.toString();
+        String time = timeeditText.toString();
 
 
         ContentValues cv = new ContentValues();
